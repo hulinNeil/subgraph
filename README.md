6b90c6c1b925d6218232ddccbb1e738afb78b4af
hulin <1289739946@qq.com>
2022-08-01 14:33:10 +0800

    正确获得token image url(以 hardcode 的方式)

--- README.md
+++ README.md
@@ -7,10 +7,14 @@
 
 ### ABI
 
+##### AzukiToken
+- Address: 0xed5af388653567af2f388e6224dc7c4b3241c544
+- 需要记录的事件：
+    - Transfer: 有人交易 Token;
+    - Mint: 第一次 mint Token;
 
 ### 注意事项
 
 1. `src` 中每个文件的函数方法都需要有调用的地方，每个函数都需要声明类型，否则会部署失败.
 2. `handler` 中的函数不能使用箭头函数，否则部署失败，提示是函数找不到
 3. 代码中不能有 `console.log`
-
--- src/modules/token.ts
+++ src/modules/token.ts
@@ -1,18 +1,20 @@
-import { Token } from "../../generated/schema";
-import { AzukiToken, Transfer } from "../../generated/Token/AzukiToken";
-import { addTokenCount } from "./count";
+import { Token } from '../../generated/schema';
+import { AzukiToken, Transfer } from '../../generated/Token/AzukiToken';
+import { addTokenCount } from './count';
 
-const AzukiAddress = "0xed5af388653567af2f388e6224dc7c4b3241c544";
+const AzukiIdPrefix = 'Azuki #';
+const AzukiTokenImagePrefix = 'https://ikzttp.mypinata.cloud/ipfs/QmYDvPAXtiJg7s8JdRBSLWdgSphQdac8j1YuQNNxcGE1hg/';
+const AzukiAddress = '0xed5af388653567af2f388e6224dc7c4b3241c544';
 
 export function updateAzukiToken(event: Transfer): void {
-  const ID = event.params.tokenId.toString() + AzukiAddress;
+  const ID = AzukiIdPrefix + event.params.tokenId.toString();
 
   let token = Token.load(ID);
   if (!token) {
     token = new Token(ID);
     token.tokenId = event.params.tokenId;
     token.tokenAddress = AzukiAddress;
-    token.tokenImage = "11";
+    token.tokenImage = AzukiTokenImagePrefix + event.params.tokenId.toString() + '.png';
     const tokenContract = AzukiToken.bind(event.address);
     token.tokenUrl = tokenContract.tokenURI(event.params.tokenId);
     addTokenCount();
